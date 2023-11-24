39a50af4cd14fc27bf30cd518963b8fc7d6927c5
LuisGabriel2603 <luisgabriel2603@gmail.com>
2023-11-23 22:38:42 -0300

    feat: atualizando as interfaces de cadastro de produto

--- src/interfaces/createProduct.interface.ts
+++ src/interfaces/createProduct.interface.ts
@@ -1,6 +1,22 @@
 export interface ICreateProduct {
     nome: string;
-    descricao: string;
-    preco: number;
-    estoque: number;
+    dataFabricacao: Date;
+    dataValidade: Date;
+    quantidade: number
+    valorUnitario: number;
+    valorTotal: number;
+    perecivel: boolean;
 }
+
+// model Produto {
+//     id                 Int       @id @default(autoincrement())
+//     nome               String    // Nome do Produto
+//     dataFabricacao     DateTime  // Data de fabricação
+//     dataVencimento     DateTime  // Data de vencimento
+//     quantidade         Int       // Quantidade do produto
+//     valorUnitario      Float     // Valor unitário
+//     valorTotal         Float     // Valor total
+//     perecivel          Boolean   // Produto perecível ou não
+//     createdAt          DateTime  @default(now())
+//     updatedAt          DateTime  @updatedAt
+//   }
